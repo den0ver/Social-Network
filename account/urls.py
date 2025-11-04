ec0630b8146d746536d06de1f67a1efe5041861e
Denys Ilyashenko <dlalernen@gmail.com>
2025-11-04 07:02:35 +0100

    Fixed issues from previous commit

--- account/urls.py
+++ account/urls.py
@@ -11,20 +11,20 @@ urlpatterns = [
     #path('login/', views.user_login, name='login'),
 
     #URL-адреса для входа и выхода
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     #URL-адреса для смены пароля
-    #path('password-change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')), name="password_change"),
-    #path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path('password-change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')), name="password_change"),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
 
-    #path('', views.dashboard, name='dashboard'),
 
     #URL-адреса для сброса пароля
-    #path('password-reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    #path('passsword-reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    #path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    #path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('account:password_reset_done')), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('account:password_reset_complete')), name="password_reset_confirm"),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
-    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
 ]
--- bookmark/settings.py
+++ bookmark/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-EMAIL_BACKEND =  'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+
 
 LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
